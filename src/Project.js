022769c85deab96067d4d8881126bbde0ef31431
Reza <reza1377maleki@gmail.com>
2022-11-01 18:32:16 +0330

    update and fix New Project section

--- src/Project.js
+++ src/Project.js
@@ -1,16 +1,13 @@
 // create a default(factory function) for any project you want to add later
-export default function Project(name , description , dueDate, type){
-    let projectName = name
-    let projectDescription = description;
-    let projectDueDate = dueDate;
-    let projectType = type
+export default function Project(projectName , projectDescription, projectType){
+    let name= projectName
+    let description = projectDescription;
+    let type = projectType
 
-    return {projectName , projectDescription , projectDueDate , projectType}
+    return {name , description , type}
 }
 // make a list to store each project in it
 export const projectArray = []
-const project1 = Project("todo-list", "make a web application for managing your todo's and projects",
-'',"Work");
-const project2 = Project("Advanced JavaScript", "Read all the Advanced topics in Odin Project",
-"1 month", "educate");
+const project1 = Project("todo-list", "make a web application for managing your todo's and projects","Work");
+const project2 = Project("Advanced JavaScript", "Read all the Advanced topics in Odin Project", "educate");
 projectArray.push(project1,project2);
--- src/user-Interface.js
+++ src/user-Interface.js
@@ -1,33 +1,42 @@
 import Project from "./Project.js";
+import { projectArray } from "./Project.js";
 
 // display projects on main screen
 export default (function projectsDisplayer() {
     const projectsSection = document.querySelector("#projects");
     const mainScreen = document.querySelector(".main-screen");
     projectsSection.addEventListener("click", () => {
+        if (document.querySelector("#projectName-input")) {
+            return;
+        }
         mainScreen.innerHTML = "";
         console.log("clears main screen")
         const title = document.createElement("h2");
         title.textContent = "Projects";
         mainScreen.appendChild(title);
         addProject();
-        
+
     })
 })()
 // new project section
 let addProject = () => {
     const mainScreen = document.querySelector(".main-screen");
-    let newProject = document.createElement("div")
-    newProject.classList.add("newProject")
+    let newProject = undefined ;
+    if(document.querySelector(".newProject")){
+        newProject = document.querySelector(".newProject")
+    } else {
+        newProject = document.createElement("div");
+        newProject.classList.add("newProject");
+        mainScreen.appendChild(newProject);
+    }
     let projectName = document.createElement("input");
     projectName.type = "text";
     projectName.placeholder = "Add Project";
     projectName.id = "projectName-input"
     newProject.appendChild(projectName);
-    mainScreen.appendChild(newProject)
     addSign();
-    projectName.addEventListener("click" , projectInfo)
-    
+    projectName.addEventListener("click", projectInfo)
+
 }
 // Plus Button for adding new project
 let addSign = () => {
@@ -37,39 +46,41 @@ let addSign = () => {
     addSign.classList.add("plusSign");
     newProject.appendChild(addSign);
     projectAdder();
-    
+
 }
 // collapsing window for adding new project 
 let projectInfo = () => {
-    const newProject = document.querySelector(".newProject");
-    const projectDesc = document.createElement("input")
-    projectDesc.type = "text"
-    projectDesc.placeholder = "Add Description (Optional)";
-    projectDesc.id = "projectDesc-input";
-    const projectDate = document.createElement("input");
-    projectDate.type = "date";
-    projectDate.id = "projectDate-input";
-    const projectType = document.createElement("select");
-    projectType.name = "type";
-    projectType.id = "projectType-input";
-    // create an option for each type available
-    let types = ["Work" , "Workout", "Educate", "Personal", "Day To Day"];
-    const defaultOption = document.createElement("option");
-    defaultOption.value = "";
-    defaultOption.textContent= "Select Project Type"
-    defaultOption.setAttribute("disabled",'');
-    defaultOption.setAttribute("selected",'');
-    projectType.appendChild(defaultOption);
-    for(let i=0; i< types.length; i++ ){
-        let option = document.createElement("option");
-        option.value = types[i];
-        option.textContent = types[i];
-        projectType.appendChild(option);
+    if (document.querySelector("#projectDesc-input")) {
+        return;
+    } else {
+        const projectname = document.querySelector("#projectName-input");
+        projectname.placeholder += "(Name)";
+        const newProject = document.querySelector(".newProject");
+        const projectDesc = document.createElement("input")
+        projectDesc.type = "text"
+        projectDesc.placeholder = "Add Description (Optional)";
+        projectDesc.id = "projectDesc-input";
+        const projectType = document.createElement("select");
+        projectType.name = "type";
+        projectType.id = "projectType-input";
+        // create an option for each type available
+        let types = ["Work", "Workout", "Educate", "Personal", "Day To Day"];
+        const defaultOption = document.createElement("option");
+        defaultOption.value = "";
+        defaultOption.textContent = "Select Project Type"
+        defaultOption.setAttribute("disabled", '');
+        defaultOption.setAttribute("selected", '');
+        projectType.appendChild(defaultOption);
+        for (let i = 0; i < types.length; i++) {
+            let option = document.createElement("option");
+            option.value = types[i];
+            option.textContent = types[i];
+            projectType.appendChild(option);
+        }
+        newProject.style.height = "105px";
+        newProject.appendChild(projectDesc);
+        newProject.appendChild(projectType);
     }
-    newProject.style.height = "140px"
-    newProject.appendChild(projectDesc);
-    newProject.appendChild(projectDate);
-    newProject.appendChild(projectType);
 }
 // when plus button is clicked check for required information before adding a new project 
 let projectAdder = () => {
@@ -77,14 +88,33 @@ let projectAdder = () => {
     // disable the button if project name is empty and focus on name input
     addSign.addEventListener("click", () => {
         let projectName = document.querySelector("#projectName-input");
-        if(projectName.value == ""){
+        if (projectName.value == "") {
             projectName.focus();
             projectInfo();
+        } else {
+            let projectDesc = document.querySelector("#projectDesc-input");
+            let projectType = document.querySelector("#projectType-input")
+            let plusSign = document.querySelector(".plusSign");
+            // let typeValue = projectType.options[projectType.selectedIndex].value;
+            let project = Project(projectName.value, projectDesc.value, projectType.value);
+            projectArray.push(project)
+            closeInfo();
+            projectName.value = "";
+            projectDesc.value = "";
+            projectType.value = "";
+            // projectName.addEventListener("click", () => {
+            //     let newProject = document.querySelector(".newProject");
+            //     newProject.style.height = "105px";
+            // });
+            projectName.remove();
+            projectDesc.remove();
+            projectType.remove();
+            plusSign.remove();
+            addProject();
+            console.log(projectArray)
         }
     })
 }
-// collapsing window for adding new project
-
 // display collapsing window for each navbar item
 export let navbarController = (() => {
     const navigators = document.querySelectorAll(".nav");
@@ -94,3 +124,10 @@ export let navbarController = (() => {
         })
     }
 })()
+
+// close add project collapsing window
+function closeInfo() {
+    let newProject = document.querySelector(".newProject");
+    newProject.style.height = "35px"
+    console.log("height changed")
+}
