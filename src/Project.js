ad00a03b5c102bc506b49bfe707ba8d35880accf
Reza <reza1377maleki@gmail.com>
2022-11-12 14:24:27 +0330

    prevents tasks from overflowing in projects. and make's some changes in projects section's ui

--- src/Project.js
+++ src/Project.js
@@ -19,6 +19,10 @@ export default function Project(projectName , projectDescription, projectType){
 export const projectArray = []
 const project1 = Project("todo-list", "make a web application for managing your todo's and projects","Work");
 let project1Task = project1.addTask("new",'',true);
+let project1Task2 = project1.addTask("a serious task", '',false)
 project1.tasks.push(project1Task);
+project1.tasks.push(project1Task2)
 const project2 = Project("Advanced JavaScript", "Read all the Advanced topics in Odin Project", "educate");
+let project2Task = project2.addTask("real task", '',true)
+project2.tasks.push(project2Task);
 projectArray.push(project1,project2);
