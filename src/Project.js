ea18b7a70e578291ad2e291a2e1b35d24a0ba23d
Reza <reza1377maleki@gmail.com>
2022-11-25 19:56:40 +0330

    add a option to set a time for each task

--- src/Project.js
+++ src/Project.js
@@ -4,12 +4,13 @@ export default function Project(projectName ,projectID, projectDescription, proj
     let id = projectID;
     const description = projectDescription;
     const tag = projectType
-    const addTask = (taskName, taskDate, taskImportant) => {
+    const addTask = (taskName, taskDate, taskTime, taskImportant) => {
         const name = taskName;
         const date = taskDate;
+        const time = taskTime;
         const important = taskImportant;
         const complete = false;
-        return {name,date,important,complete}
+        return {name,date,time,important,complete}
     }
     const tasks = [];
     const complete = false;
@@ -20,11 +21,11 @@ export default function Project(projectName ,projectID, projectDescription, proj
 export const projectArray = []
 const project1 = Project("todo-list", 0, "make a web application for managing your todo's and projects","Work");
 let project1Task = project1.addTask("new",'',true);
-let project1Task2 = project1.addTask("a serious task", '',false)
+let project1Task2 = project1.addTask("a serious task", '', '',false)
 project1.ad
 project1.tasks.push(project1Task);
 project1.tasks.push(project1Task2)
 const project2 = Project("Advanced JavaScript", 1, "Read all the Advanced topics in Odin Project", "educate");
-let project2Task = project2.addTask("real task", '',true)
+let project2Task = project2.addTask("real task", '','',true)
 project2.tasks.push(project2Task);
 projectArray.push(project1,project2);
--- src/projects-page-ui.js
+++ src/projects-page-ui.js
@@ -296,13 +296,22 @@ let detailShower = () => {
             let dateContainer = document.createElement("div");
             dateContainer.classList.add("datepicker-container");
             dateContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="datepicker-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"></svg>';
+            let timeContainer = document.createElement("div");
+            timeContainer.classList.add("timepicker-container");
+            timeContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="timepicker-icon" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"></svg>';
             let options = document.createElement("div");
             options.classList.add("newTask-options");
+            let newTaskDate = document.createElement("input");
+            newTaskDate.type = "date";
+            newTaskDate.id = "newTaskDate-input";
+            newTaskDate.tabIndex = "-1";
             let newTaskTime = document.createElement("input");
-            newTaskTime.type = "date";
+            newTaskTime.type = "time";
             newTaskTime.id = "newTaskTime-input";
-            newTaskTime.tabIndex = "-1";
-            dateContainer.appendChild(newTaskTime);
+            newTaskTime.tabIndex = "-1"; 
+            timeContainer.appendChild(newTaskTime);
+            dateContainer.appendChild(newTaskDate);
+            options.appendChild(timeContainer);
             options.appendChild(dateContainer);
             // create a button that if clicked, new task inputs appear
             function addTaskButton() {
@@ -330,6 +339,9 @@ let detailShower = () => {
                         let dateIcon = document.querySelector(".datepicker-icon");
                         dateIcon.innerHTML = '<path d="M4.684 11.523v-2.3h2.261v-.61H4.684V6.801h2.464v-.61H4v5.332h.684zm3.296 0h.676V8.98c0-.554.227-1.007.953-1.007.125 0 .258.004.329.015v-.613a1.806 1.806 0 0 0-.254-.02c-.582 0-.891.32-1.012.567h-.02v-.504H7.98v4.105zm2.805-5.093c0 .238.192.425.43.425a.428.428 0 1 0 0-.855.426.426 0 0 0-.43.43zm.094 5.093h.672V7.418h-.672v4.105z"/>'
                         dateIcon.innerHTML += '<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>';
+                        let timeIcon = document.querySelector(".timepicker-icon");
+                        timeIcon.innerHTML = '<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>';
+                        timeIcon.innerHTML += '<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>'
                     }, 600);
                 })
             }
@@ -379,6 +391,7 @@ let detailShower = () => {
                     };
                     let taskName = '';
                     let taskDate = '';
+                    let taskTime = '';
                     let taskImportance = false;
                     // flip the new task's section to ask if the task is important or not
                     let flippingCard = document.querySelector(`#flipping-card${projectObject.id}`);
@@ -417,13 +430,15 @@ let detailShower = () => {
                     yesButton.addEventListener("click", () => {
                         taskImportance = true;
                         taskName = newTask.value;
-                        taskDate = newTaskTime.value;
+                        taskDate = newTaskDate.value;
+                        taskTime = newTaskTime.value;
                         taskHandler();
                     });
                     noButton.addEventListener("click", () => {
                         taskImportance = false;
                         taskName = newTask.value;
-                        taskDate = newTaskTime.value;
+                        taskDate = newTaskDate.value;
+                        taskTime = newTaskTime.value;
                         taskHandler();
                     })
                     // click yes or no buttons using enter key on keyboard
@@ -439,14 +454,14 @@ let detailShower = () => {
                     })
                     // make a function to add the task and display everything in it's place 
                     function taskHandler() {
-                        let task = projectObject.addTask(taskName, taskDate, taskImportance);
+                        let task = projectObject.addTask(taskName, taskDate, taskTime, taskImportance);
                         flippingCard.classList.remove("flipped");
                         projectObject.tasks.push(task);
                         // remove task inputs and put newtask button back in its place
                         newTaskContainer.removeChild(input_btnContainer);
                         newTaskContainer.removeChild(options);
                         newTask.value = "";
-                        newTaskTime = "";
+                        newTaskDate = "";
                         sortImportant();
                         addTaskButton();
                         display.none();
@@ -464,7 +479,7 @@ let detailShower = () => {
                     };
                 });
                 // make task adder button work on calender when you click enter key
-                newTaskTime.addEventListener("keypress", (event) => {
+                newTaskDate.addEventListener("keypress", (event) => {
                     if (event.keyCode === 13) {
                         btn.click();
                     };
