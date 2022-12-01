50b26eca7db4e276657b0934e894221770cbc93b
Reza <reza1377maleki@gmail.com>
2022-12-01 16:43:13 +0330

    change task completion animation

--- src/completeTask.js
+++ src/completeTask.js
@@ -4,13 +4,21 @@ import { displayObject } from "./projects-page-ui";
 // add a function to Task that marks a task's DOM and the task's object as complete
 export function CompleteTask(taskNode, Task) {
     taskNode.addEventListener("click", () => {
+        let nodeChild = taskNode.firstChild;
+        let tick = document.createElement("div");
+        tick.classList.add("yes")
+        tick.classList.add("completeTick");
+        nodeChild.appendChild(tick);
         taskNode.classList.add("active");
         setTimeout(() => {
-            Task.isComplete = true;
-            displayObject.none();
-            displayObject.all();
-            importantButton();
-            console.log("I DID WORK")
+            tick.classList.add("done");
+            setTimeout(() => {
+                Task.isComplete = true;
+                displayObject.none();
+                displayObject.all();
+                importantButton();
+                console.log("I DID WORK")
+            }, 600);
         }, 1000);
     })
 }
