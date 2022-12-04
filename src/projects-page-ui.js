1113d3f3a7246a4cd762083809e90148cb7c141a
Reza <reza1377maleki@gmail.com>
2022-12-04 13:37:38 +0330

    update and clean 3dots functions

--- src/projects-page-ui.js
+++ src/projects-page-ui.js
@@ -531,7 +531,6 @@ let detailShower = () => {
 }
 // make each 3dot edit button functional
 let editDots = () => {
-    let container = document.querySelector(".Projects-ul");
     let dotsButtons = document.querySelectorAll(".edit-span");
     let openDots = "";                           // it contains the id number of 3dot edit button that is currently open
     for (let i = 0; i < dotsButtons.length; i++) {
@@ -545,50 +544,52 @@ let editDots = () => {
                 document.querySelector(".layout-container").remove();
                 document.querySelector(openDots).classList.toggle("open");
             };
-            dotsButtons[i].classList.add("open");
-            openDots = `#eSpan${i}`;
-            let layoutContainer = document.createElement("div");
-            layoutContainer.classList.add("layout-container");
-            layoutContainer.id = `layout${i}`;
-            container.appendChild(layoutContainer);
-            layoutContainer.style["grid-area"] = `${i + 1} / 2 / span 1 / span 1`
-            let triangle = document.createElement("div");
-            triangle.classList.add("triangle");
-            layoutContainer.appendChild(triangle);
-            let mainBox = document.createElement("div");
-            mainBox.classList.add("edit-mainBox");
-            layoutContainer.appendChild(mainBox);
-            let delButton = document.createElement("div");
-            delButton.classList.add("del-button");
-            delButton.classList.add("option");
-            let removeIcon = document.createElement("div");
-            removeIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-            <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-          </svg>`;
-            removeIcon.classList.add("icon");
-            delButton.appendChild(removeIcon);
-            mainBox.appendChild(delButton);
-            let completeButton = document.createElement("div");
-            completeButton.classList.add("complete-button");
-            completeButton.classList.add("option");
-            let doneIcon = document.createElement("div")
-            doneIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
-            <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
-            <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
-          </svg>`;
-            doneIcon.classList.add("icon");
-            completeButton.appendChild(doneIcon);
-            mainBox.appendChild(completeButton);
-
+            openDots = displayOptions(i);
         })
     }
 
 
 }
 // display edit options for 3dot edit button
-let disOptions = () => {
-
+let displayOptions = (index) => {
+    let container = document.querySelector(".Projects-ul");
+    let dotsButtons = document.querySelectorAll(".edit-span");
+    dotsButtons[index].classList.add("open");
+    let openDots = `#eSpan${index}`;
+    let layoutContainer = document.createElement("div");
+    layoutContainer.classList.add("layout-container");
+    layoutContainer.id = `layout${index}`;
+    container.appendChild(layoutContainer);
+    layoutContainer.style["grid-area"] = `${index + 1} / 2 / span 1 / span 1`
+    let triangle = document.createElement("div");
+    triangle.classList.add("triangle");
+    layoutContainer.appendChild(triangle);
+    let mainBox = document.createElement("div");
+    mainBox.classList.add("edit-mainBox");
+    layoutContainer.appendChild(mainBox);
+    let delButton = document.createElement("div");
+    delButton.classList.add("del-button");
+    delButton.classList.add("option");
+    let removeIcon = document.createElement("div");
+    removeIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+  </svg>`;
+    removeIcon.classList.add("icon");
+    delButton.appendChild(removeIcon);
+    mainBox.appendChild(delButton);
+    let completeButton = document.createElement("div");
+    completeButton.classList.add("complete-button");
+    completeButton.classList.add("option");
+    let doneIcon = document.createElement("div")
+    doneIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+  </svg>`;
+    doneIcon.classList.add("icon");
+    completeButton.appendChild(doneIcon);
+    mainBox.appendChild(completeButton);
+    return openDots
 }
 
 // make a function to get and store new taaks
